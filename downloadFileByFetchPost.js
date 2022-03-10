51bb3ba391f503bc630e51e26d9ab8c4ed89c64f
filipempinto <filipemourapinto@gmail.com>
2022-03-10 11:04:17 -0300

    Update downloadFileByFetchPost.js

--- downloadFileByFetchPost.js
+++ downloadFileByFetchPost.js
@@ -12,14 +12,22 @@ await fetch(urlToObtainFile, {
             var url = window.URL.createObjectURL(blob);
             
             window.open(url);
-            //to force download comment //windou.open(url) and uncomment following lines
+            //to force download comment //window.open(url) and uncomment following lines
             
+	//----alternative 1--------------
             //var a = document.createElement('a');
             //a.href = url;
             //a.download = "filename.pdf";
             //document.body.appendChild(a); //  the element to the dom -> otherwise it will not work in firefox
             //a.click();    
             //a.remove();  //afterwards we remove the element again         
+	//---- end alt 1-----------
+	
+	//----alternative 2---------------
+	//var file = window.URL.createObjectURL(blob);
+    	//window.location.assign(file);
+	//---- end alt 2-----------
+	
         });
         
 /*  some tips at:
